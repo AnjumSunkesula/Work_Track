7b407008f2efc463b0e25b144400c9bc35d995be
afroz <afrozdd7@gmail.com>
2026-01-11 17:03:51 +0530

    added routes for needed files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Register from './pages/Register';
+import Login from './pages/login';
+import ProtectedRoute from "./components/ProtectedRoute";
 import './App.css'
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
 
   return (
-    <>
-      <div>hello</div>
-    </>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <h1>Dashboard</h1>
+                <LogoutButton/>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
-
 export default App
