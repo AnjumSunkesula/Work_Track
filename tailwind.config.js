3fa8349c5efcfb5c387e6187c762b7eea59e1d40
afroz <afrozdd7@gmail.com>
2026-01-19 13:31:42 +0530

    Tailwindcss installation and paackages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: "#B77466",
+          light: "#FFE1AF",
+          accent: "#E2B59A",
+          dark: "#957C62",
+        }, 
+      },
+    },
   },
   plugins: [],
 }
