616a3e860784da33309b30881aaca1966819d7ee
afroz <afrozdd7@gmail.com>
2026-01-29 14:43:14 +0530

    passed 'classname="' as a props

--- src/components/auth/AuthCard.jsx
+++ src/components/auth/AuthCard.jsx
@@ -1,17 +1,6 @@
-export default function AuthCard({ children }) {
+export default function AuthCard({ children, className="" }) {
   return (
-    <div
-      className="
-        w-full max-w-xl
-        rounded-2xl
-        bg-brand-secondary/20
-        backdrop-blur-xl
-        border border-white/30
-        shadow-2xl
-        p-8
-        text-white
-      "
-    >
+    <div className={`w-full max-w-xl rounded-2xl bg-brand-secondary/20 backdrop-blur-xl border border-white/30 shadow-2xl p-8  text-white ${className}`}>
       {children}
     </div>
   );
