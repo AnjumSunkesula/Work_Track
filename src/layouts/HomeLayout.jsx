9c9decbfc29b6a96fd02a41d2880dcc161de0d4e
afroz <afrozdd7@gmail.com>
2026-02-09 14:05:39 +0530

    modified screen height and text color to green

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -5,7 +5,7 @@ import LiquidChrome from "../components/background/LiquidChrome";
 export default function HomeLayout() {
   const [focused, setFocused] = useState(false);
   return (
-    <div className="relative min-h-screen w-full overflow-hidden">
+    <div className="relative min-h-[100svh] w-full overflow-x-hidden">
       
       {/* Liquid Chrome Background */}
       <div className="absolute inset-0 -z-10">
@@ -21,37 +21,37 @@ export default function HomeLayout() {
       <div className="absolute inset-0 bg-black/30 -z-10" />
 
       {/* Split Layout */}
-      <div className="relative z-10 min-h-screen grid grid-cols-1 sm:grid-cols-2">
+      <div className="relative z-10 min-h-[100svh] flex flex-col sm:grid sm:grid-cols-2">
 
         {/* 🔹 MOBILE CONTEXT HEADER */}
         <div
           className={`
-            sm:hidden
-            flex flex-col items-center text-center
-            px-6 pt-10 pb-6
-            text-white
+            flex sm:hidden
+            flex-col items-center text-center
+            px-6 mt-10 mb-6
+            text-green-950
             transition-all duration-300
             ${focused ? "opacity-60 scale-95" : ""}
           `}
         >
-          <h1 className="mb-3 inline-block font-bold text-2xl">
+          <h1 className="mb-3 inline-block font-bold text-3xl">
             Work Track
           </h1>
 
-          <h2 className="text-xl font-bold leading-tight">
-            Work smarter.<br />
+          <h2 className="text-base font-semibold leading-tight">
+            Work smarter,<br />
             Stay organised.
           </h2>
         </div>
         
         {/* LEFT SIDE – Marketing */}
-        <div className="hidden sm:flex flex-col justify-center px-16 tracking-wide text-white">
+        <div className="hidden sm:flex flex-col justify-center px-16 tracking-wide text-green-950">
           <span className="mb-4 inline-block text-lg font-bold">
             Work Track
           </span>
 
-          <h1 className="text-3xl lg:text-5xl font-semibold leading-tight">
-            Work smarter.<br />
+          <h1 className="text-3xl lg:text-4xl font-semibold leading-tight">
+            Work smarter,<br />
             Stay organised.
           </h1>
 
@@ -71,10 +71,9 @@ export default function HomeLayout() {
         </div>
 
         {/* RIGHT SIDE – Auth */}
-        <div className="flex items-center justify-center px-6 bg-transparent pb-9 sm:pb-20 md:pb-0">
+        <div className="flex items-start sm:items-center justify-center px-6 bg-transparent py-9 md:pb-0">
           <Outlet context={{ setFocused}}/>
         </div>
-
       </div>
     </div>
   );
